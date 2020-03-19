f9d9eda6e7ecfb3d30adb9b20b377f185b1c4e9a
blessyou <370732889@qq.com>
2020-03-19 11:26:44 +0800

    refactor: 更新google auth

--- api/oauth2/v4/token/index.go
+++ api/oauth2/v4/token/index.go
@@ -1,10 +1,21 @@
 package handler
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
+// 代理google oauth
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	req, _ := http.NewRequest("POST", "https://www.googleapis.com/oauth2/v4/token", bytes.NewBuffer(reqBody))
+	req.Header.Set("accept", "application/json")
+	client := &http.Client{}
+	client.Timeout = 15 * time.Second
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Fprintf(w, string(body))
 }
