95f7f93b86d979e0ebb218913e562f825e146146
blessyou <370732889@qq.com>
2020-03-19 11:12:28 +0800

    refactor: 更新google auth

--- api/tokeninfo/index.go
+++ api/tokeninfo/index.go
@@ -2,9 +2,18 @@ package handler
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+	url := fmt.Sprintf("https://oauth2.googleapis.com/tokeninfo?%s", r.URL.RawQuery)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("accept", "application/json")
+	client := &http.Client{}
+	client.Timeout = 15 * time.Second
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Fprintf(w, string(body))
 }
